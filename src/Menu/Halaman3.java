02b079adc4089f1e291d592840052a39b5753720
FayadAl-Habsyi <fayedalhabsyi636@gmail.com>
2021-06-19 20:05:21 +0800

    membuat 6 objek untuk 3 construktor

--- src/Menu/Halaman3.java
+++ src/Menu/Halaman3.java
@@ -1,5 +1,12 @@
 package Menu;
 
 public class Halaman3 {
-
+	public static void main(String [] args) {
+		Halaman1 H1 = new Halaman1("Fayad Al-Habsyi","19631086","4C Ti Bjb","20");
+		Halaman1 H2 = new Halaman1();
+		Halaman1 H3 = new Halaman1();
+		Halaman1 H4 = new Halaman1(null);
+		Halaman1 H5 = new Halaman1(null);
+		Halaman1 H6 = new Halaman1(null);
+}
 }
