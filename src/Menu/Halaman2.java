187eb898ec530c64ce4921c15b7cae8c840635a3
FayadAl-Habsyi <fayedalhabsyi636@gmail.com>
2021-06-19 20:06:53 +0800

    membuat get&set pada Halaman2

--- src/Menu/Halaman2.java
+++ src/Menu/Halaman2.java
@@ -1,5 +1,38 @@
 package Menu;
 
-public class Halaman2 {
-
+public class Halaman2 extends Halaman1 {
+	//atribut
+		private int a;
+		private int b;
+		private String c;
+		
+		//2 method
+		public void hobi() {
+			String hobi="ngoding";
+			System.out.println("Hobi  : "+super.getnama());
+		}
+		public void mainw() {
+				
+		}
+		
+		//membuat getter dan setter
+		public int geta() {
+			return a;
+		}
+		public void seta() {
+			this.a = a;
+		}
+		
+		public int getb() {
+			return b;
+		}
+		public void setb() {
+			this.b = b;
+		}
+		public String getc() {
+			return c;
+		}
+		public void setc() {
+			this.c = c;
+		}
 }
